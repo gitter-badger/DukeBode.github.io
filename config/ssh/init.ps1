2442b7878f39372674eea098808c76a935f10df2
DukeBode <Duke123@aliyun.com>
2019-08-31 12:11:14 +0800

    Site updated: 2019-08-31 12:10:30

---
+++ config/ssh/init.ps1
@@ -0,0 +1,85 @@
+
+$config = "config11"
+echo $config > hello
+ = $config + "\hello"
+
+# ssh 目录
+$ssh = $home + "\.ssh"
+
+# config 文件
+$config = $ssh + "\config"
+
+# 仓库私钥路径
+$aliyun = $ssh + "\aliyun"
+$coding = $ssh + "\coding"
+$gitee = $ssh + "\gitee"
+$github = $ssh + "\github"
+$huawei = $ssh + "\huawei"
+$azure = $ssh + '\azure'
+
+echo $home 
+
+echo github
+# ehco "# github" >> hello
+# # https://github.com/
+# Host github.com
+#     HostName github.com
+#     PreferredAuthentications publickey
+#     IdentityFile C:\Users\Duke1\.ssh\github
+
+# # aliyun
+# # https://code.aliyun.com/
+# Host code.aliyun.com
+#     HostName code.aliyun.com
+#     PreferredAuthentications publickey
+#     IdentityFile C:\Users\Duke1\.ssh\aliyun
+pause
+# # coding
+# # https://coding.net/
+# Host git.coding.net
+#     HostName git.coding.net
+#     PreferredAuthentications publickey
+#     IdentityFile C:\Users\Duke1\.ssh\coding
+
+# # coding BY tencent
+# # https://dev.tencent.com/u/DukeBode
+# Host git.dev.tencent.com
+#     HostName git.dev.tencent.com
+#     PreferredAuthentications publickey
+#     IdentityFile C:\Users\Duke1\.ssh\coding
+
+# # gitee
+# # https://gitee.com/
+# Host gitee.com
+#     HostName gitee.com
+#     PreferredAuthentications publickey
+#     IdentityFile C:\Users\Duke1\.ssh\gitee
+
+# # azure
+# # https://dev.azure.com
+# Host ssh.dev.azure.com
+#     HostName ssh.dev.azure.com
+#     PreferredAuthentications publickey
+#     IdentityFile C:\Users\Duke1\.ssh\azure
+
+# # azure & visualstudio
+# Host vs-ssh.visualstudio.com
+#     HostName vs-ssh.visualstudio.com
+#     PreferredAuthentications publickey
+#     IdentityFile C:\Users\Duke1\.ssh\azure
+
+# # huaweicloud codehub
+# # https://devcloud.huaweicloud.com/codehub
+# Host codehub.devcloud.huaweicloud.com
+#     HostName codehub.devcloud.huaweicloud.com
+#     PreferredAuthentications publickey
+#     IdentityFile C:\Users\Duke1\.ssh\huawei
+# ssh-keygen -t rsa -C "Duke123@aliyun.com" -f "C:\Users\Duke1\.ssh\github"
+
+# ssh-keygen -t rsa -C "Duke123@aliyun.com" -f "C:\Users\Duke1\.ssh\github"
+# ssh-keygen -t rsa -C "Duke123@aliyun.com" -f "C:\Users\Duke1\.ssh\github"
+# ssh-keygen -t rsa -C "Duke123@aliyun.com" -f "C:\Users\Duke1\.ssh\github"
+# ssh-keygen -t rsa -C "Duke123@aliyun.com" -f "C:\Users\Duke1\.ssh\github"
+# ssh-keygen -t rsa -C "Duke123@aliyun.com" -f "C:\Users\Duke1\.ssh\github"
+# ssh-keygen -t rsa -C "Duke123@aliyun.com" -f "C:\Users\Duke1\.ssh\github"
+# ssh-keygen -t rsa -C "Duke123@aliyun.com" -f "C:\Users\Duke1\.ssh\github"
