61eae98591cc13133417fe3a2f6e8bebbe046c6a
DukeBode <Duke123@aliyun.com>
2019-12-16 13:40:13 +0800

    update

--- source/config/ssh/init.ps1
+++ source/config/ssh/init.ps1
@@ -20,60 +20,6 @@ $azure = $ssh + '\azure'
 echo $home 
 
 echo github
-# ehco "# github" >> hello
-# # https://github.com/
-# Host github.com
-#     HostName github.com
-#     PreferredAuthentications publickey
-#     IdentityFile C:\Users\Duke1\.ssh\github
-
-# # aliyun
-# # https://code.aliyun.com/
-# Host code.aliyun.com
-#     HostName code.aliyun.com
-#     PreferredAuthentications publickey
-#     IdentityFile C:\Users\Duke1\.ssh\aliyun
-pause
-# # coding
-# # https://coding.net/
-# Host git.coding.net
-#     HostName git.coding.net
-#     PreferredAuthentications publickey
-#     IdentityFile C:\Users\Duke1\.ssh\coding
-
-# # coding BY tencent
-# # https://dev.tencent.com/u/DukeBode
-# Host git.dev.tencent.com
-#     HostName git.dev.tencent.com
-#     PreferredAuthentications publickey
-#     IdentityFile C:\Users\Duke1\.ssh\coding
-
-# # gitee
-# # https://gitee.com/
-# Host gitee.com
-#     HostName gitee.com
-#     PreferredAuthentications publickey
-#     IdentityFile C:\Users\Duke1\.ssh\gitee
-
-# # azure
-# # https://dev.azure.com
-# Host ssh.dev.azure.com
-#     HostName ssh.dev.azure.com
-#     PreferredAuthentications publickey
-#     IdentityFile C:\Users\Duke1\.ssh\azure
-
-# # azure & visualstudio
-# Host vs-ssh.visualstudio.com
-#     HostName vs-ssh.visualstudio.com
-#     PreferredAuthentications publickey
-#     IdentityFile C:\Users\Duke1\.ssh\azure
-
-# # huaweicloud codehub
-# # https://devcloud.huaweicloud.com/codehub
-# Host codehub.devcloud.huaweicloud.com
-#     HostName codehub.devcloud.huaweicloud.com
-#     PreferredAuthentications publickey
-#     IdentityFile C:\Users\Duke1\.ssh\huawei
 # 在用户下的.ssh目录下运行
 ssh-keygen -t rsa -C "Duke123@aliyun.com" -f aliyun
 ssh-keygen -t rsa -C "Duke123@aliyun.com" -f aliyun_ecs
