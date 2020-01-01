8de99e8d618323d88ea20ecb5e767ff74bd445cc
DukeBode <Duke123@aliyun.com>
2020-01-01 22:06:40 +0800

    Site updated: 2020-01-01 22:06:02

--- config/ssh/init.ps1
+++ config/ssh/init.ps1
@@ -20,66 +20,12 @@ $azure = $ssh + '\azure'
 echo $home 
 
 echo github
-# ehco "# github" >> hello
-# # https://github.com/
-# Host github.com
-#     HostName github.com
-#     PreferredAuthentications publickey
-#     IdentityFile C:\Users\Duke1\.ssh\github
+# 在用户下的.ssh目录下运行
+ssh-keygen -t rsa -C "Duke123@aliyun.com" -f aliyun
+ssh-keygen -t rsa -C "Duke123@aliyun.com" -f aliyun_ecs
+ssh-keygen -t rsa -C "Duke123@aliyun.com" -f azure
+ssh-keygen -t rsa -C "Duke123@aliyun.com" -f coding
+ssh-keygen -t rsa -C "Duke123@aliyun.com" -f gitee
+ssh-keygen -t rsa -C "Duke123@aliyun.com" -f github
+ssh-keygen -t rsa -C "Duke123@aliyun.com" -f huawei
 
-# # aliyun
-# # https://code.aliyun.com/
-# Host code.aliyun.com
-#     HostName code.aliyun.com
-#     PreferredAuthentications publickey
-#     IdentityFile C:\Users\Duke1\.ssh\aliyun
-pause
-# # coding
-# # https://coding.net/
-# Host git.coding.net
-#     HostName git.coding.net
-#     PreferredAuthentications publickey
-#     IdentityFile C:\Users\Duke1\.ssh\coding
-
-# # coding BY tencent
-# # https://dev.tencent.com/u/DukeBode
-# Host git.dev.tencent.com
-#     HostName git.dev.tencent.com
-#     PreferredAuthentications publickey
-#     IdentityFile C:\Users\Duke1\.ssh\coding
-
-# # gitee
-# # https://gitee.com/
-# Host gitee.com
-#     HostName gitee.com
-#     PreferredAuthentications publickey
-#     IdentityFile C:\Users\Duke1\.ssh\gitee
-
-# # azure
-# # https://dev.azure.com
-# Host ssh.dev.azure.com
-#     HostName ssh.dev.azure.com
-#     PreferredAuthentications publickey
-#     IdentityFile C:\Users\Duke1\.ssh\azure
-
-# # azure & visualstudio
-# Host vs-ssh.visualstudio.com
-#     HostName vs-ssh.visualstudio.com
-#     PreferredAuthentications publickey
-#     IdentityFile C:\Users\Duke1\.ssh\azure
-
-# # huaweicloud codehub
-# # https://devcloud.huaweicloud.com/codehub
-# Host codehub.devcloud.huaweicloud.com
-#     HostName codehub.devcloud.huaweicloud.com
-#     PreferredAuthentications publickey
-#     IdentityFile C:\Users\Duke1\.ssh\huawei
-# ssh-keygen -t rsa -C "Duke123@aliyun.com" -f "C:\Users\Duke1\.ssh\github"
-
-# ssh-keygen -t rsa -C "Duke123@aliyun.com" -f "C:\Users\Duke1\.ssh\github"
-# ssh-keygen -t rsa -C "Duke123@aliyun.com" -f "C:\Users\Duke1\.ssh\github"
-# ssh-keygen -t rsa -C "Duke123@aliyun.com" -f "C:\Users\Duke1\.ssh\github"
-# ssh-keygen -t rsa -C "Duke123@aliyun.com" -f "C:\Users\Duke1\.ssh\github"
-# ssh-keygen -t rsa -C "Duke123@aliyun.com" -f "C:\Users\Duke1\.ssh\github"
-# ssh-keygen -t rsa -C "Duke123@aliyun.com" -f "C:\Users\Duke1\.ssh\github"
-# ssh-keygen -t rsa -C "Duke123@aliyun.com" -f "C:\Users\Duke1\.ssh\github"
