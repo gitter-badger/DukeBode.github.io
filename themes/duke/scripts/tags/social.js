271ad66fe5dd00c63b43808ef6c69bb43edd31c5
DukeBode <Duke123@aliyun.com>
2020-02-18 23:14:40 +0800

    update

---
+++ themes/duke/scripts/tags/social.js
@@ -0,0 +1,13 @@
+// Size: medium,large
+hexo.extend.helper.register('linkedin', function() {
+    // console.log(hexo.config)
+    console.log(hexo.data)
+
+    // size = 'medium' || 'large'; //主280||330，侧250||300
+    // type = 'horizontal' || 'vertical'; // 主框架 || 侧边栏
+    // theme = 'light' || 'dark';
+    // href = 'https://cn.linkedin.com/in/dukebode?trk=profile-badge';
+    // return `<div class=${div_class||'LI-profile-badge'}  data-version=${version||'v1'} data-size=${size||'zh_CN'} data-locale=${locale} data-type=${type} data-theme=${theme} data-vanity=${vanity||'dukebode'}>
+    //     <a class=${a_class||'LI-simple-link'} href=${href}>${content||'博德蝈蝈'}</a>
+    // </div>`
+});
